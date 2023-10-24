f2f9831993c7b9a67257f27fcec2cc4bf00a443b
Leonardo S Matos <leonardo.matos@unimednatal.com.br>
2023-10-24 18:12:01 -0300

    Implementação de leitura, criação, remoção e edição de tarefas

---
+++ src/routes.js
@@ -0,0 +1,84 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search
+      } : null)
+
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString()
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const foundedTask = database.findById('tasks', id)
+
+      if (!Object.keys(foundedTask).length) {
+        return res.writeHead(404).end()
+      }
+
+      database.delete('tasks', id)
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const foundedTask = database.findById('tasks', id)
+
+      if (!Object.keys(foundedTask).length) {
+        return res.writeHead(404).end()
+      }
+
+      Object.entries(req.body).some(([key, value]) => {
+        foundedTask[key] = value
+      })
+
+      database.update('tasks', id, {
+        ...foundedTask,
+        updated_at: new Date().toISOString()
+      })
+
+      return res.writeHead(204).end()
+    }
+  }
+]
